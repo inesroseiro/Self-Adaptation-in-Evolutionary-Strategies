f920625221f72247e29a556acc7bb9ff3b28c83c
manofrancisco <francisco4martinsferreira@gmail.com>
2020-05-21 12:34:12 +0100

    Merge branch 'master' of /Users/franciscoferreira/Desktop/CE/Self-Adaptation-in-Evolutionary-Strategies with conflicts.

--- functions_adaptative.py
+++ functions_adaptative.py
@@ -18,41 +18,41 @@ def merito_s(indiv):
     return schwefel(fenotipo(indiv))
 
 def merito_q(indiv):
-	return quartic(fenotipo(indiv))
+    return quartic(fenotipo(indiv))
 
 def merito_step(indiv):
     return step(fenotipo(indiv))
     
 def merito_rosenbrock(indiv):
-	return rosenbrock(fenotipo(indiv))
+    return rosenbrock(fenotipo(indiv))
 
 def merito_griewangk(indiv):
-	return griewangk(fenotipo(indiv))
+    return griewangk(fenotipo(indiv))
 
 def fenotipo(indiv):
     return indiv[0]  
 
 def sphere(indiv):
     """ De Jong F1 or the sphere function
-	domain: [-5.12, 5.12] for each dimension.
-	min = 0 at x = (0,0,...,0)
-	"""
+    domain: [-5.12, 5.12] for each dimension.
+    min = 0 at x = (0,0,...,0)
+    """
     return sum([ x ** 2 for x in indiv])
 
 
 def rosenbrock(indiv):
-	"""
-	De Jong F2 or Rosembrock function
-	domain: [-2.048, 2.048] for each dimension.
-	min = 0 at x = (0,0,...,0)
-	"""
-	soma = 0
-	for i in range(len(indiv)-1):
-		soma+= (1-indiv[i])**2 + 100*(indiv[i+1]-(indiv[i])**2)**2
-	return soma
+    """
+    De Jong F2 or Rosembrock function
+    domain: [-2.048, 2.048] for each dimension.
+    min = 0 at x = (0,0,...,0)
+    """
+    soma = 0
+    for i in range(len(indiv)-1):
+        soma+= (1-indiv[i])**2 + 100*(indiv[i+1]-(indiv[i])**2)**2
+    return soma
 
 def step(indiv):
-	""" 
+	"""
 	De Jong F3 or the step function
 	domain: [-5.12, 5.12] for each dimension.
 	min = 0 at x = (-5.12,-5.12,...,-5.12)
@@ -64,13 +64,13 @@ def step(indiv):
 	return 6*tamanho +soma
 
 def quartic(indiv):
-	"""
-	quartic = DeJong 4
-	domain = [-1.28; 1.28]
-	minimum 0 at x = 0
-	"""
-	y = sum([ (i+1) * x**4 for i,x in enumerate(indiv)]) + random.uniform(0,1)
-	return y
+    """
+    quartic = DeJong 4
+    domain = [-1.28; 1.28]
+    minimum 0 at x = 0
+    """
+    y = sum([ (i+1) * x**4 for i,x in enumerate(indiv)]) + random.uniform(0,1)
+    return y
 
 def rastrigin(indiv):
     """
@@ -88,7 +88,6 @@ def schwefel(indiv):
 	domain = [-500; 500]
 	minimum at (420.9687,...,420.9687)
 	"""
-
 	tamanho = len(indiv)
 
 	y = 418.9829 * tamanho - sum([-x * math.sin(math.sqrt(math.fabs(x))) for x in indiv])
@@ -96,64 +95,64 @@ def schwefel(indiv):
 
 # based on https://www.cs.unm.edu/~neal.holts/dga/benchmarkFunction/griewank.html
 def griewangk(indiv):
-	"""
-	F6 Griewank's function
-  	domain = [-600; 600]
+    """
+    F6 Griewank's function
+    domain = [-600; 600]
     minimum at (0, ..., 0)
     """
-	p1 = 0
-	p2 = 1
-	for i in range(len(indiv)):
-		p1 += (indiv[i])**2
-		p2 *= np.cos(indiv[i] / math.sqrt(i+1))
-	return 1 + p1/4000 + p2
+    p1 = 0
+    p2 = 1
+    for i in range(len(indiv)):
+        p1 += (indiv[i])**2
+        p2 *= np.cos(indiv[i] / math.sqrt(i+1))
+    return 1 + p1/4000 + p2
 
 
 def get_params(func, size_cromo):
-	if(func == 'quartic'):
-		domain = [[-1.28,1.28]]*size_cromo
-		amp_sigma = (domain[0][1]-domain[0][0])/10
-		sigma = [round(random.uniform(0,amp_sigma),2)  for i in range(size_cromo)]
-		return domain, sigma, merito_q
-	elif(func=='rastrigin'):
-		domain = [[-5.12,5.12]]*size_cromo
-		amp_sigma = (domain[0][1]-domain[0][0])/10
-		sigma = [round(random.uniform(0,amp_sigma),2)  for i in range(size_cromo)]
-		return domain, sigma, merito_r
-	elif(func=='schwefel'):
-		domain = [[-500,500]]*size_cromo
-		amp_sigma = (domain[0][1]-domain[0][0])/100
-		sigma = [round(random.uniform(0,amp_sigma),2)  for i in range(size_cromo)]
-		return domain, sigma, merito_s
-	elif(func=='sphere'):
-		domain = [[-5.12,5.12]]*size_cromo
-		amp_sigma = (domain[0][1]-domain[0][0])/10
-		sigma = [round(random.uniform(0,amp_sigma),2)  for i in range(size_cromo)]
-		return domain, sigma, merito_sphere
-	elif(func=='rosenbrock'):
-		domain = [[-5.12,5.12]]*size_cromo
-		amp_sigma = (domain[0][1]-domain[0][0])/10
-		sigma = [round(random.uniform(0,amp_sigma),2)  for i in range(size_cromo)]
-		return domain, sigma, merito_rosenbrock
-	elif(func=='step'):
-		domain = [[-5.12,5.12]]*size_cromo
-		amp_sigma = (domain[0][1]-domain[0][0])/10
-		sigma = [round(random.uniform(0,amp_sigma),2)  for i in range(size_cromo)]
-		return domain, sigma, merito_step
-	elif(func=='griewangk'):
-		domain = [[-600,600]]*size_cromo
-		amp_sigma = (domain[0][1]-domain[0][0])/100
-		sigma = [round(random.uniform(0,amp_sigma),2)  for i in range(size_cromo)]
-		return domain, sigma, merito_griewangk
-	else:
-		print('Error: unrecognized benchmark function <'+func+'>')
-		exit()
+    if(func == 'quartic'):
+        domain = [[-1.28,1.28]]*size_cromo
+        amp_sigma = (domain[0][1]-domain[0][0])/10
+        sigma = [round(random.uniform(0,amp_sigma),2)  for i in range(size_cromo)]
+        return domain, sigma, merito_q
+    elif(func=='rastrigin'):
+        domain = [[-5.12,5.12]]*size_cromo
+        amp_sigma = (domain[0][1]-domain[0][0])/10
+        sigma = [round(random.uniform(0,amp_sigma),2)  for i in range(size_cromo)]
+        return domain, sigma, merito_r
+    elif(func=='schwefel'):
+        domain = [[-500,500]]*size_cromo
+        amp_sigma = (domain[0][1]-domain[0][0])/100
+        sigma = [round(random.uniform(0,amp_sigma),2)  for i in range(size_cromo)]
+        return domain, sigma, merito_s
+    elif(func=='sphere'):
+        domain = [[-5.12,5.12]]*size_cromo
+        amp_sigma = (domain[0][1]-domain[0][0])/10
+        sigma = [round(random.uniform(0,amp_sigma),2)  for i in range(size_cromo)]
+        return domain, sigma, merito_sphere
+    elif(func=='rosenbrock'):
+        domain = [[-5.12,5.12]]*size_cromo
+        amp_sigma = (domain[0][1]-domain[0][0])/10
+        sigma = [round(random.uniform(0,amp_sigma),2)  for i in range(size_cromo)]
+        return domain, sigma, merito_rosenbrock
+    elif(func=='step'):
+        domain = [[-5.12,5.12]]*size_cromo
+        amp_sigma = (domain[0][1]-domain[0][0])/10
+        sigma = [round(random.uniform(0,amp_sigma),2)  for i in range(size_cromo)]
+        return domain, sigma, merito_step
+    elif(func=='griewangk'):
+        domain = [[-600,600]]*size_cromo
+        amp_sigma = (domain[0][1]-domain[0][0])/100
+        sigma = [round(random.uniform(0,amp_sigma),2)  for i in range(size_cromo)]
+        return domain, sigma, merito_griewangk
+    else:
+        print('Error: unrecognized benchmark function <'+func+'>')
+        exit()
     
     
 if __name__ == '__main__':
-	pass
-	
+    pass
 
-	    
-	    
+
+
+
 
